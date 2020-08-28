e01de45c1bf1ba3c51e5e0f38a1bdebcad7a6b42
Scott Wilfong <nikeaa@gmail.com>
2020-08-28 17:44:07 -0500

    Break out model and converter classes

---
+++ STTDataAnalyzer/Models/StoredImmortal.cs
@@ -0,0 +1,16 @@
+﻿namespace STTDataAnalyzer
+{
+	namespace SttUser
+	{
+		using Newtonsoft.Json;
+
+		public partial class StoredImmortal
+		{
+			[JsonProperty("id")]
+			public long Id { get; set; }
+
+			[JsonProperty("quantity")]
+			public long Quantity { get; set; }
+		}
+	}
+}
---
+++ STTDataAnalyzer/Models/Welcome.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace STTDataAnalyzer
+{
+
+	namespace SttUser
+	{
+		public class Welcome
+		{
+			public static Welcome FromJson(string json)
+			{
+				return JsonConvert.DeserializeObject<Welcome>(json, SttUser.Converter.Settings);
+			}
+
+			[JsonProperty("action")]
+			public string Action { get; set; }
+
+			[JsonProperty("player")]
+			public Player Player { get; set; }
+
+			[JsonProperty("item_archetype_cache")]
+			public ItemArchetypeCache ItemArchetypeCache { get; set; }
+		}
+	}
+}
